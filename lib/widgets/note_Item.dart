14fc5ae80340d83fc35f78c632e3c1832f313bb7
ahmed wael <ahmed855wael@gmail.com>
2024-08-14 02:37:25 +0300

    add edit notes view

--- lib/widgets/note_Item.dart
+++ lib/widgets/note_Item.dart
@@ -1,48 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/view/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 22, bottom: 22, left: 16),
-      decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 247, 207, 86),
-          borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: Text(
-                'Biuld your career with Ahmed wael',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.6),
-                  fontSize: 18,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 22, bottom: 22, left: 16),
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 247, 207, 86),
+            borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  'Biuld your career with Ahmed wael',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.6),
+                    fontSize: 18,
+                  ),
                 ),
               ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 22,
+                  )),
             ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 22,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Text('Augast 13,2024',
-                style: TextStyle(color: Colors.black.withOpacity(0.6))),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Text('Augast 13,2024',
+                  style: TextStyle(color: Colors.black.withOpacity(0.6))),
+            )
+          ],
+        ),
       ),
     );
   }
