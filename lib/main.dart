2da8a17cd3d1e8738f2716a46d2374897ed0d1b5
ahmed wael <ahmed855wael@gmail.com>
2024-08-16 00:53:04 +0300

    add hive package , coutom model and genrate modelAdapter.

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,7 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark ,
-      fontFamily: 'Poppins'),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
       home: const NotesView(),
     );
   }
