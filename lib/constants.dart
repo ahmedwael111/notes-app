1368afef647c8caf5d94a553d17cfff631402e9c
ahmed wael <ahmed855wael@gmail.com>
2024-08-20 09:58:38 +0300

    Finish Edit view Color list ====> Finish this beautiful App <====

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,10 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color.fromARGB(255, 33, 229, 243);
 const kBoxNote = 'Box_note';
+List<Color> kcolorList = [
+  const Color(0xffEAE2B7),
+  const Color(0xffFCBF49),
+  const Color(0xffF77F00),
+  const Color(0xffD62828),
+  const Color.fromARGB(255, 3, 63, 96)
+];
